e7adbfe3711bbf0cb5b1262818c03823b4e42807
guechanlee <guechanlee@gmailcom>
2026-08-25 17:29:44 +0900

    20260825 memo

--- src/main/java/com/office/toypjt/ToyPjtConfig.java
+++ src/main/java/com/office/toypjt/ToyPjtConfig.java
@@ -2,4 +2,10 @@ package com.office.toypjt;
 
 public class ToyPjtConfig {
 
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String URL = "jdbc:mysql://localhost:3306/DB_MEMO";
+	public static final String USER = "root";
+	public static final String PASSWORD = "1234";
+	
+	
 }
--- src/main/java/com/office/toypjt/memo/MemoDao.java
+++ src/main/java/com/office/toypjt/memo/MemoDao.java
@@ -1,5 +1,65 @@
 package com.office.toypjt.memo;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.office.toypjt.ToyPjtConfig;
+
 public class MemoDao {
 
+	public int insertMemo(MemoDto memoDto) {
+		System.out.println("[MemoDao]insertMemo()");
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int result = -1;
+		
+		try {
+			Class.forName(ToyPjtConfig.DRIVER);
+			
+			conn = DriverManager.getConnection(
+					ToyPjtConfig.URL,
+					ToyPjtConfig.USER,
+					ToyPjtConfig.DRIVER
+					);
+			
+			String sql = "INSERT INTO tbl_memo("
+					+ "mem_id, "
+					+ "memo_title, "
+					+ "memo_conment) "
+					+ "VALUES(?, ?, ?)";
+
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, memoDto.getMemId());
+			pstmt.setString(1, memoDto.getMemoTitle());
+			pstmt.setString(1, memoDto.getMemoComent());
+			
+			result = pstmt.executeUpdate();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				
+				if (pstmt != null)
+					pstmt.close();
+				
+				if (conn != null)
+					conn.close();
+				
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return result;
+		
+	}
+	
 }
