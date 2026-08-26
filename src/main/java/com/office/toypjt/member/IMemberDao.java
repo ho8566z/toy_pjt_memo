cc29728a10479ec2b7debd6eb24289fc621985e9
seh020515 <seh020515@gmail.com>
2026-08-26 10:52:14 +0900

    20260826 001

--- src/main/java/com/office/toypjt/member/IMemberDao.java
+++ src/main/java/com/office/toypjt/member/IMemberDao.java
@@ -2,10 +2,15 @@ package com.office.toypjt.member;
 
 public interface IMemberDao {
 	
-//	public int insertNewMember(MemberDto memberDto);
+	public final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	public final String URL = "jdbc:mysql://localhost:3306/db_memo";
+	public final String USER ="root";
+	public final String PASSWORD = "1234";
+	
+	public int insertNewMember(MemberDto memberDto);
 
-//	public MemberDto selectMemberByMemId(String memId);
+	public MemberDto selectMemberByMemId(String memId);
 
-//	public int updateMemberByMemNo(MemberDto memberDto);s
+	public int updateMemberByMemNo(MemberDto memberDto);
 
 }
--- src/main/java/com/office/toypjt/member/MemberDao.java
+++ src/main/java/com/office/toypjt/member/MemberDao.java
@@ -1,7 +1,48 @@
 package com.office.toypjt.member;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class MemberDao implements IMemberDao {
 	
 	final private String CLASS_NAME = "[MemberDao] ";
 	
+	@Override
+	public int insertNewMember(MemberDto memberDto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int result = -1;
+	
+
+		
+		try {
+			Class.forName(DRIVER);
+			
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+			String sql = "INSERT INTO tbl_member(memId, memPw, memMail, memPhone)"
+					+ "VALUES(?, ? , ?, ?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, memberDto.getMemId());
+			pstmt.setString(2, memberDto.getMemPw());
+			pstmt.setString(3, memberDto.getMemMail());
+			pstmt.setString(4, memberDto.getMemPhone());
+			
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+				
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
+	}
 }
